e95093dc6faf0f31eebac59a9640aa45cb234cfc
ttyt0416 <ttyt0416@gmail.com>
2021-04-15 17:05:08 +0900

    adding bottom bar, filtering screen, side drawer, etc

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/main_drawer.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filters';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MainDrawer(),
+    );
+  }
+}
